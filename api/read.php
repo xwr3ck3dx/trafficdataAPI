6ac2a78138fa421f739a228f8dc5941ce32bac30
xwr3ck3dx <gamer_sa5m@hotmail.com>
2020-02-07 13:51:20 +0545

    api

---
+++ api/read.php
@@ -0,0 +1,41 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../config/Database.php';
+    include_once '../models/Post.php';
+
+    $database = new Database();
+    $db= $database->connect();
+
+    $trafficdata=new Post($db);
+
+    $result = $trafficdata->read();
+    $num = $result->rowCount();
+
+    if($num > 0){
+
+        $trafficdata_arr = array();
+        $trafficdata_arr['data']= array();
+
+        while($row = $result->fetch(PDO::FETCH_ASSOC){
+            extract($row);
+            $trafficdata_item =array(
+                'ip' => $id,
+                'date' => $date,
+                'inBytes' => $inBytes,
+                'outBytes' => $outBytes
+            );
+            array_push($trafficdata['data'],$trafficdata_item);
+        }
+
+        echo json_encode($trafficdata_arr);
+
+    } else {
+        echo json_encode(
+            array('message'=> 'No data Found')
+        );
+    }
+
+
+?>
---
+++ models/Trafficdata.php
@@ -0,0 +1,29 @@
+<?php
+    class Trafficdata {
+        private $conn;
+        private $table = 'Daily';
+
+
+        public $date;
+        public $ip;
+        public $inBytes;
+        public $outBytes;
+
+
+
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+        public function read() {
+            $query = 'SELECT ip, date, inBytes, outBytes from '.$this->table.' ORDER BY date INC';
+
+            $stmt= $this->conn->prepare($query);
+
+            $stmt->execute();
+
+            return $stmt;
+        }
+    }
+
+?>
