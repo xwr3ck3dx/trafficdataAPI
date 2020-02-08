0ee004d28b6bc2b501b0f404b2ddd6c115b816e2
xwr3ck3dx <gamer_sa5m@hotmail.com>
2020-02-08 14:33:27 +0545

    fixes

--- chart.php
+++ chart.php
@@ -11,8 +11,8 @@
 
 <html>
 <head>  
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css">
- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.min.js"></script>
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.css">
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
 </head>
 <body>
 <div class="container">
