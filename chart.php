4a15f7612e059a60ff0f4bd859f5f0d57fbf97f7
xwr3ck3dx <gamer_sa5m@hotmail.com>
2020-02-08 14:26:06 +0545

    fixes

--- chart.php
+++ chart.php
@@ -15,6 +15,10 @@
  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.min.js"></script>
 </head>
 <body>
+<div class="container">
+<canvas id="myChart" height="400" width="400"></canvas>
+
+</div>
 
 <canvas id="myChart"></canvas>
 <script>
