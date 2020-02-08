e0b0dc38a4d1b2c6f227cdb3540b95ca6c7e16ad
xwr3ck3dx <gamer_sa5m@hotmail.com>
2020-02-08 12:13:14 +0545

    fixes

--- chart.php
+++ chart.php
@@ -17,32 +17,36 @@
 <canvas id="myChart" width="400" height="400"></canvas>
 <script>
 	var ctx = document.getElementById('myChart');
+	var data = {
+	    labels: [
+			<?php foreach($response['data'] as $data){ ?>
+				"<?php echo $data['date']?>",
+			<?php }?>
+		],
+		    datasets: [
+				{
+					label: "Blue",
+					backgroundColor: "blue",
+					data: [
+						<?php foreach($response['data'] as $data){ ?>
+							"<?php echo $data['inBytes']?>",
+						<?php }?>
+					]
+				},
+				{
+					label: "Red",
+					backgroundColor: "red",
+					data: [
+						<?php foreach($response['data'] as $data){ ?>
+							"<?php echo $data['outBytes']?>",
+						<?php }?>
+					]
+				},
+			]
+	};
 	var myChart = new Chart(ctx, {
 		type: 'bar',
-		data: {
-			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-			datasets: [{
-				label: '# of Votes',
-				data: [12, 19, 3, 5, 2, 3],
-				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
-				],
-				borderColor: [
-					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
-				],
-				borderWidth: 1
-			}]
-		},
+		data: data,
 		options: {
 			scales: {
 				yAxes: [{
